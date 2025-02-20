9211fb052fe7b9988b24c42166d2460588f4ea0a
shkhrtrxsh <shkhrtrxsh@gmail.com>
2025-02-21 01:59:07 +0530

    Add dotenv for environment configuration and database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://shkhrtrxsh:mongodb@cluster0.kad9e.mongodb.net/test"
-  );
+  await mongoose.connect(process.env.DATABASE_URL);
   console.log("Connected to MongoDB");
 };
 
