2ca3b6f3be5fd83dbd2d5e392be5b3ff45e4f272
shkhrtrxsh <shkhrtrxsh@gmail.com>
2025-02-22 01:59:03 +0530

    created /user and /feed get APIs

--- src/app.js
+++ src/app.js
@@ -16,6 +16,29 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 connectDB()
   .then(() => {
     app.listen(3000, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  email: {
+  emailId: {
     type: String,
     required: true,
     unique: true,
