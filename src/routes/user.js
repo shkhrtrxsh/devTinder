a5d0967ac1b4f71722bbaedb69c5662793382f54
shkhrtrxsh <shkhrtrxsh@gmail.com>
2025-03-01 02:35:16 +0530

    Implement pagination for user feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -67,6 +67,12 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = Math.min(limit, 50);
+    const skip = (page - 1) * limit;
+
     const connectionRequests = await ConnectionRequestModel.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select("fromUserId toUserId");
@@ -81,7 +87,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select("firstName lastName age photoUrl skills");
+    })
+      .select("firstName lastName age photoUrl skills")
+      .skip(skip)
+      .limit(limit);
 
     res.status(200).json({ message: "Feed", users });
   } catch (error) {
