b73cce0f839805d39673ef8442413e95b118210b
shkhrtrxsh <shkhrtrxsh@gmail.com>
2025-03-06 21:40:07 +0530

    added db url

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DATABASE_URL);
+  await mongoose.connect(
+    "mongodb+srv://shkhrtrxsh:mongodb@cluster0.kad9e.mongodb.net/"
+  );
   console.log("Connected to MongoDB");
 };
 
