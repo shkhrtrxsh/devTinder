94ad8ec358434918d300355e4741ee80829cef1f
shkhrtrxsh <shkhrtrxsh@gmail.com>
2025-03-07 16:23:37 +0530

    Replace hardcoded secrets with environment variables
    - Updated database connection to use DATABASE_URL from environment
    - Replaced hardcoded JWT secret with JWT_SECRET from environment
    - Improved security by removing sensitive credentials from source code

--- src/models/user.js
+++ src/models/user.js
@@ -75,7 +75,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.methods.getJWT = async function () {
   const user = this;
-  const token = await jwt.sign({ userId: this._id }, "DEV@Tinder$790", {
+  const token = await jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
   return token;
