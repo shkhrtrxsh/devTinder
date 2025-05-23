adcf6121ebd998e5328fca0fdb7beb86ec98e3d8
shkhrtrxsh <shkhrtrxsh@gmail.com>
2025-05-23 09:53:39 +0530

    updated origin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dev-tinder-web-five.vercel.app",
     credentials: true,
   })
 );
